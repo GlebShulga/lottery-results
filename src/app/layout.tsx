45edc6763a9d326593c9582ca7092669532472ca
GlebShulga <Shulga_Gleb@hotmail.com>
2024-03-08 10:48:33 +0400

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "../components/Header";
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Header />
         {children}
       </body>
+      <GoogleAnalytics gaId="G-CEB3WHSVDK" />
     </html>
   );
 }
